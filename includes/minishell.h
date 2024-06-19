9ed99d37992c173a23ddef2700c0a76e6851c131
mariaserpa <iza-serpa@hotmail.com>
2024-06-19 16:20:31 +0200

    Add libraries and headers in files

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,31 @@
-#ifndef MINISHELL
-#define MINISHELL
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 16:06:36 by mrabelo-          #+#    #+#             */
+/*   Updated: 2024/06/19 16:17:00 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <stdio.h>
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/errno.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <signal.h>
+# include <string.h>
+# include <limits.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 16:17:19 by mrabelo-          #+#    #+#             */
+/*   Updated: 2024/06/19 16:17:55 by mrabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/minishell.h"
 
-int main(void)
+int	main(void)
 {
-    printf("Walcome to Maluzinha's and Vanessinha's Minishell\n");
-    return (0);
+	printf("Welcome to Maluzinha's and Vanessinha's Minishell\n");
+	return (0);
 }
