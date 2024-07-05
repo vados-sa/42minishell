08ceb36186623e679b3053b522fdd6acd31fc60d
Vanessa Dos Santos <vados-sa@c3a8c5.42berlin.de>
2024-07-05 17:13:52 +0200

    starting lexing

--- src/1_lexer/lexer.c
+++ src/1_lexer/lexer.c
@@ -6,8 +6,23 @@
 /*   By: vados-sa <vados-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:09:51 by vados-sa          #+#    #+#             */
-/*   Updated: 2024/07/02 16:10:40 by vados-sa         ###   ########.fr       */
+/*   Updated: 2024/07/05 16:59:30 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+void    lexical_analysis(t_data *data, char *input)
+{
+	
+}
+
+int main(void)
+{
+	char *rl;
+	t_data  *data;
+
+	rl = "grep -i 'search term' < input.txt | sort > output.txt";
+	lexical_analysis(data, rl);
+	return (0);
+}
