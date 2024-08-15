6fb3c12a8a6936b7ba1981934facce4645f488e0
Vanessa dos Santos <vanessasantos@Vanessas-MBP.fritz.box>
2024-08-15 10:12:47 +0200

    it was all wrong lol

--- src/2_parsing/handle_arg_for_export.c
+++ src/2_parsing/handle_arg_for_export.c
@@ -6,7 +6,7 @@
 /*   By: vados-sa <vados-sa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/10 18:37:06 by vados-sa          #+#    #+#             */
-/*   Updated: 2024/08/12 16:25:28 by vados-sa         ###   ########.fr       */
+/*   Updated: 2024/08/14 18:57:08 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,20 @@
 
 int	handle_export_builtin_arg(t_command *cmd_node, t_token *token)
 {
-	int	i;
+	
+	return (EXIT_SUCCESS);
+}
 
-	i = 0; 
-	if (!ft_isalpha(token->value[i]) && token->value[i] != '_') // invalid indetifier error
+// CHECK FOR INVALID IDENTIFIERS
+// this may go in the execution part.
+/* if (!ft_isalpha(token->value[i]) && token->value[i] != '_') // invalid indetifier error
 	{
 		printf("bash: export: `%s': not a valid identifier\n", token->value);
 		return (EXIT_FAILURE);
 	}
-	while (token->value[i]) // FIND '=' OR 'space'
+	while (token->value[i] || token->value[i] != '=') // FIND '=' OR 'space'
 	{
-		if (token->value[i] == '=')
-			break ;
-		else if (!ft_isalnum(token->value[i]) && token->value[i] != '_')
+		if (!ft_isalnum(token->value[i]) && token->value[i] != '_')
 			return (EXIT_FAILURE); // invalid identifier error
 		i++;
-	}
-	if (token->value[i])
-		; // save variable with no value to be exported -> I don't fully understand this yet
-	else if (token->value[i] == '=')
-	{
-		; //save the variable name -> save node
-		i++;
-		//save 2nd node, value of the variable.
-	}
-	return (EXIT_SUCCESS);
-}
+	} */
