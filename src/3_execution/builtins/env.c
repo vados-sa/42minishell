ae93eed1ac3faba00e7393a781ba2483ef9e1c5e
Vanessa dos Santos <vanessasantos@Vanessas-MacBook-Pro.fritz.box>
2024-09-04 15:51:42 +0200

    update so env only prints valued variables

--- src/3_execution/builtins/env.c
+++ src/3_execution/builtins/env.c
@@ -3,15 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: vados-sa <vados-sa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 23:20:43 by mrabelo-          #+#    #+#             */
-/*   Updated: 2024/08/31 15:41:55 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2024/09/04 15:50:41 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
 
+// CHECK IF QUOTES ARE BEING PRINTED -> IT SHOULDN'T !
+
+/**
+ * @brief Checks if the given variable has an equal sign, indicating it is 
+ * a valid environment variable for `env`.
+ * 
+ * @param variable The environment variable string (e.g., "VAR=value" or "VAR=").
+ * @return 1 if the variable has an equal sign, 0 otherwise.
+ */
+
+static int	has_an_equal_sign(char *variable)
+{
+	int	i;
+
+	i = 0;
+	while(variable[i])
+	{
+		if (variable[i] == '=')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
+ * @brief Builtin function to display environment variables.
+ * 
+ * The env command outputs each environment variable that has a value.
+ * If the command has any arguments or flags, it outputs an error message.
+ * 
+ * @param cmd The current command node.
+ * @param data The main data structure containing environment variables.
+ * @return EXIT_SUCC on success, EXIT_FAIL on failure.
+ */
 int	builtin_env(t_command *cmd, t_data *data)
 {
 	int	i;
@@ -19,12 +53,14 @@ int	builtin_env(t_command *cmd, t_data *data)
 	i = 0;
 	if (cmd->flags || cmd->arguments)
 	{
-		ft_putstr_fd("minishell: env don't support options or arguments\n", 1); //check if it is 1 or 2
+		ft_putstr_fd("minishell: env doesn't support options or arguments\n", \
+			STDERR_FILENO);
 		return (EXIT_FAIL);
 	}
 	while (data->env[i])
 	{
-		printf("%s\n", data->env[i]);
+		if (has_an_equal_sign(data->env[i]))
+			printf("%s\n", data->env[i]);
 		i++;
 	}
 	return (EXIT_SUCC);
