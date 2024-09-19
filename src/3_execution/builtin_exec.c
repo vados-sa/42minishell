f6cd099745548755806c04dda994a421d22a13fc
Vanessa Dos Santos <vados-sa@c3a7c2.42berlin.de>
2024-09-19 14:31:44 +0200

    removed chat gbt change

--- src/3_execution/builtin_exec.c
+++ src/3_execution/builtin_exec.c
@@ -3,43 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exec.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: vados-sa <vados-sa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 15:44:07 by mrabelo-          #+#    #+#             */
-/*   Updated: 2024/09/16 17:32:31 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2024/09/19 14:29:06 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-
-//code that chatgpt provided
-int process_builtin(int **fds, int pos, t_command *cmd, t_data *data) {
-    int fd[2];
-    int exit_code;
-
-    fd[0] = dup(STDIN_FILENO);
-    fd[1] = dup(STDOUT_FILENO);
-    if (fd[0] == -1 || fd[1] == -1)
-        return (EXIT_FAIL);
-
-    if (redirect_io(fds, pos, data, ft_lstsize_mod(data->command)) == EXIT_FAIL)
-        return (EXIT_FAIL);
-
-    exit_code = execute_builtin(cmd, data);  // Execute the built-in command
-
-    if (dup2(fd[0], STDIN_FILENO) == -1 || dup2(fd[1], STDOUT_FILENO) == -1)
-        return (EXIT_FAIL);
-
-    close_fd(&fd[0]);
-    close_fd(&fd[1]);
-
-    data->exit_status = exit_code;
-    return (exit_code);
-}
-
-
-/* int	process_builtin(int **fds, int pos, t_command *cmd, t_data *data)
+int	process_builtin(int **fds, int pos, t_command *cmd, t_data *data)
 {
 	int	fd[2];
 	int	exit_code;
@@ -58,7 +31,7 @@ int process_builtin(int **fds, int pos, t_command *cmd, t_data *data) {
 	close_fd(&fd[1]);
 	data->exit_status = exit_code;
 	return (exit_code);
-} */
+}
 
 int	execute_builtin(t_command *cmd, t_data *data)
 {
