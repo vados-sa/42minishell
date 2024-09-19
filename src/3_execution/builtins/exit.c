9bb91e5c99c4b63a65f26bb4cbf787e1c42dd4b5
Vanessa Dos Santos <vados-sa@c3a7c2.42berlin.de>
2024-09-19 12:06:03 +0200

    fixed some error messages; changed fd1 to fd2; exit still needs one implementation

--- src/3_execution/builtins/exit.c
+++ src/3_execution/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: vados-sa <vados-sa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 23:20:43 by mrabelo-          #+#    #+#             */
-/*   Updated: 2024/09/19 11:48:42 by vados-sa         ###   ########.fr       */
+/*   Updated: 2024/09/19 12:03:08 by vados-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ int	builtin_exit(t_command *cmd, t_data*data)
 {
 	if (cmd->flags)
 	{
+		//printf("Argument: %s\n", cmd->flags->content);
 		if (!ft_isnumeric(cmd->flags->content) || cmd->flags->next)
 		{
 			ft_putstr_fd("minishell: exit does not support options\n", 2);
