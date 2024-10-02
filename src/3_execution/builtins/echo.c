fa5b7d1ccd8be21f8775528538777153a4404011
mariaserpa <iza-serpa@hotmail.com>
2024-10-02 16:50:06 +0200

    fix error

--- src/3_execution/builtins/echo.c
+++ src/3_execution/builtins/echo.c
@@ -3,14 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrabelo- <mrabelo-@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: malu <malu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 23:20:43 by mrabelo-          #+#    #+#             */
-/*   Updated: 2024/10/01 17:40:46 by mrabelo-         ###   ########.fr       */
+/*   Updated: 2024/10/02 16:48:21 by malu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static void	count_quotes(char *str, int *count, char quote_type)
+{
+	int i;
+
+	i = 0;
+	*count = 0;
+	while (str[i])
+	{
+		if (str[i] == quote_type)
+			(*count)++;
+		i++;
+	}
+}
+
+static void	copy_without_quotes(char *str, int *src_pos, int *dst_pos, char quote_type)
+{
+	while (str[*src_pos])
+	{
+		if (str[*src_pos] == quote_type)
+		{
+			(*src_pos)++;
+			continue;
+		}
+		str[(*dst_pos)++] = str[(*src_pos)++];
+	}
+}
+
+static void	remove_even_quotes(char *str, char quote_type)
+{
+	int src_pos;
+	int dst_pos;
+	int count;
+
+	count_quotes(str, &count, quote_type);
+	src_pos = 0;
+	dst_pos = 0;
+	if (ft_iseven(count))
+		copy_without_quotes(str, &src_pos, &dst_pos, quote_type);
+	else
+	{
+		while (str[src_pos])
+			str[dst_pos++] = str[src_pos++];
+	}
+	str[dst_pos] = '\0';
+}
 
 int	builtin_echo(t_command *cmd)
 {
@@ -25,6 +71,8 @@ int	builtin_echo(t_command *cmd)
 	temp = cmd->arguments;
 	while (temp)
 	{
+		remove_even_quotes(temp->content, SINGLE_Q);
+		remove_even_quotes(temp->content, DOUBLE_Q);
 		ft_putstr_fd(temp->content, 1);
 		if (temp->next)
 			ft_putstr_fd(" ", 1);
